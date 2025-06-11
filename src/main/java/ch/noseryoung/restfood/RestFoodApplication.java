dab101c3ab086a45c0aaa659230f4077d49ed58b
David-Tarlos <david.tarlos@edu.tbz.ch>
2025-06-11 10:07:24 +0200

    add projects setup

---
+++ src/main/java/ch/noseryoung/restfood/RestFoodApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.restfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/noseryoung/restfood/RestFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.restfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
